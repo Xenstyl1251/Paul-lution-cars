d4c2fb00ea512175c950c26b7781b412232b99a3
unknown <{your_email}>
2025-06-11 22:56:00 -0700

    almost done I added some keyframe(fade-in) when video is played

--- src/Components/Background/Background.jsx
+++ src/Components/Background/Background.jsx
@@ -8,16 +8,16 @@ import image3 from "../../assets/image3.png";
 const Background = ({ playStatus, heroCount }) => {
   if (playStatus) {
     return (
-      <video className="Background" autoPlay loop muted>
+      <video className="Background fade-in" autoPlay loop muted>
         <source src={video1} type="video/mp4" />
       </video>
     );
   } else if (heroCount === 0) {
-    return <img src={image1} className="Background" alt="" />;
+    return <img src={image1} className="Background fade-in" alt="" />;
   } else if (heroCount === 1) {
-    return <img src={image2} className="Background" alt="" />;
+    return <img src={image2} className="Background fade-in" alt="" />;
   } else if (heroCount === 2) {
-    return <img src={image3} className="Background" alt="" />;
+    return <img src={image3} className="Background fade-in" alt="" />;
   }
 };
 
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,9 +1,15 @@
 import "./Hero.css";
 import arrow_btn from "../../assets/arrow_btn.png";
-import play_btn from "../../assets/play_btn.png";
-import pause_btn from "../../assets/pause_icon.png";
+import pause_icon from "../../assets/pause_icon.png";
+import play_icon from "../../assets/play_icon.png";
 
-const Hero = ({ heroData, setHeroCount, heroCount }) => {
+const Hero = ({
+  heroData,
+  setHeroCount,
+  heroCount,
+  setPlayStatus,
+  playStatus,
+}) => {
   return (
     <div className="hero">
       <div className="hero-text">
@@ -31,6 +37,15 @@ const Hero = ({ heroData, setHeroCount, heroCount }) => {
             className={heroCount === 2 ? "hero-dot orange" : "hero-dot"}
           ></li>
         </ul>
+
+        <div className="hero-play">
+          <img
+            onClick={() => setPlayStatus(!playStatus)}
+            src={playStatus ? pause_icon : play_icon}
+            alt=""
+          />
+          <p>See the Video</p>
+        </div>
       </div>
     </div>
   );
